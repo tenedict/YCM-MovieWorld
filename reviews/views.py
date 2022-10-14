75d473c03f7e2523dcbc25577050a9d91a1c583b
hyejinny97 <yhj970102@gmail.com>
2022-10-14 16:53:40 +0900

    Create reviews/detail

--- reviews/views.py
+++ reviews/views.py
@@ -16,11 +16,21 @@ def create(request):
     if request.method == 'POST':
         review_form = ReviewForm(request.POST)
         if review_form.is_valid():
-            review_form.save()
-            return redirect('reviews:index')
+            review = review_form.save()
+            return redirect('reviews:detail', review.pk)
     else:
         review_form = ReviewForm()
     context = {
         "review_form": review_form
     }
     return render(request, 'reviews/create.html', context)
+
+# 작성글 상세 페이지
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    
+    context = {
+        'review': review,
+    }
+
+    return render(request, 'reviews/detail.html', context)
